8b4fcdc7d5457864878c966abe4fac7a5581fb3e
yliu138repo <105641046+yliu138repo@users.noreply.github.com>
2023-08-09 03:12:45 +0000

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-3">
       </div>
       <div class="col-lg-6">
-        <at-button @click="link('Rating')" icon="icon-star" class="mid-btn" type="primary"  hollow>Start Rating</at-button>
+        <at-button @click="link('Rating')" icon="icon-star" class="mid-btn" type="primary"  hollow>Start Rating Welcome</at-button>
       </div>
       <div class="col-lg-6">
         <at-button @click="link('Leaderboard')" icon="icon-bar-chart-2" class="mid-btn" type="success" hollow>View Leaderboard</at-button>
